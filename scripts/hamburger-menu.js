26ec6a3ebbfebf4517321399975920a5b3d27ab7
Vitaliy Budkin <bvfromru@gmail.com>
2021-09-20 20:31:28 +0400

    fix: delete unwanted files

--- scripts/hamburger-menu.js
+++
@@ -1,7 +0,0 @@
-let hamburgerButton = document.getElementById("burger-toggle");
-hamburgerButton.addEventListener("click", myFunction);
-
-function myFunction() {
-  let hamburgerMenu = document.getElementById("burger-menu");
-  hamburgerMenu.classList.toggle("open");
-}
